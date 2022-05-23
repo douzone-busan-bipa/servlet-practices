e5e4661b0aecf286698d4f0930b48f4899aa5813
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-23 12:21:31 +0900

    emaillist01 #1

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -12,6 +12,8 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
 		String name = request.getParameter("name");
 		
 		response.setContentType("text/html; charset=utf-8");
