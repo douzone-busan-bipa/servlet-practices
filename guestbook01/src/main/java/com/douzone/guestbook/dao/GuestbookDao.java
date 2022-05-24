64d3b228ebc1d9781e398594e2fd7d6229a88480
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-24 11:39:58 +0900

    guestbook02 #1

--- guestbook01/src/main/java/com/douzone/guestbook/dao/GuestbookDao.java
+++ guestbook01/src/main/java/com/douzone/guestbook/dao/GuestbookDao.java
@@ -147,7 +147,7 @@ public class GuestbookDao {
 		Connection conn = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
+			String url = "jdbc:mysql://192.168.10.55:3306/webdb?characterEncoding=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
