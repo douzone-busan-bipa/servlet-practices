82081a39560bb25151aaf789da5b198f1ecdbd05
더존비즈온 <103369445+douzone-busan-bipa@users.noreply.github.com>
2022-05-25 15:50:15 +0900

    Update GuestbookServlet.java

--- guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
+++ guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
@@ -8,10 +8,44 @@ import javax.servlet.http.HttpServletResponse;
 
 public class GuestbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		response.getWriter().println("<h1>OK!</h1>");
+		
+		String action = request.getParameter("a");
+		
+		if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setNo(Long.parseLong(no));
+			vo.setPassword(password);
+			
+			new GuestbookDao().delete(vo);
+			response.sendRedirect(request.getContextPath() + "/gb");
+		} else if("add".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String message = request.getParameter("message");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setMessage(message);
+			
+			new GuestbookDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/gb");
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
