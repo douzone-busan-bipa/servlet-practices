53efc6b9b41ad9197fa362cb24dcc5e8624ef998
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-09 16:13:44 +0900

    쿼리 추가

--- sql/guestbook.sql
+++ sql/guestbook.sql
@@ -1,3 +1,16 @@
 -- guestbook
 
 desc guestbook;
+
+		  select no,
+		  		 name,
+		  		 date_format(reg_date, '%Y/%m/%d %H:%i:%s') as regDate,
+		  		 message
+		    from guestbook
+		order by reg_date desc;
+        
+insert
+		  into guestbook
+		values (null, 'dwqdqw', '1234', 'hi~', now());
+
+select last_insert_id();        
